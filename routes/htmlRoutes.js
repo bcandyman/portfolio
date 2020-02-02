3719f74fb3ad8be58ca177592eab746346c2f1b8
Benjamin Candy <bcandyman93@gmail.com>
2020-02-01 21:16:57 -0800

    Added express server

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = (app) => {
+  // Load index page
+  app.get('/', (req, res) => {
+    res.render('index');
+    // res.sendFile(path.join(__dirname, '/../views/index.html'));
+  });
+
+  // Load portfolio page
+  app.get('/portfolio', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../views/portfolio.html'));
+  });
+
+  // Load contact page
+  app.get('/contact', (req, res) => {
+    res.sendFile(path.join(__dirname, '/../views/contact.html'));
+  });
+
+  // Render 404 page for any unmatched routes
+  app.get('*', (req, res) => {
+    res.render('404');
+  });
+};
